7ef974dcde7287f4b7e45f77a50694d6760e39d5
Loraine <loraine@lorainekv.com>
2015-07-15 10:22:43 -0700

    Worked on user rspec and validation for presence of username and email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :products
   has_many :orders
+
+  validates :username, presence: true
+  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "Requires that a username must be present" do
+      user = User.new
+
+      expect(user).to_not be_valid
+      expect(user.errors.keys).to include(:username)
+  end
+
+
+  it "Requires that a email must be present" do
+      user = User.new
+
+      expect(user).to_not be_valid
+      expect(user.errors.keys).to include(:email)
+  end
 
 end
