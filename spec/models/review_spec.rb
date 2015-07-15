8fbe68a7d4c16b87decac08213f765dac7ee90a3
Loren Wang <wang131.131@gmail.com>
2015-07-15 10:21:56 -0700

    review model validations & tests

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   describe "model validations" do
-    it "requires rating to be an integer" do
-      review = Review.new(rating: "a string")
+    it "requires a rating" do
+      review = Review.new
       expect(review).to_not be_valid
+      expect(review.errors.keys).to include(:rating)
     end
 
+    it "rating must be an integer" do
+      review = Review.new(rating: "")
+      expect(review).to_not be_valid
+      expect(review.errors.keys).to include(:rating)
+    end
+
+    it "requires rating to be between 1 - 5" do
+      review = Review.new(rating: 5)
+      expect(review).to be_valid
+    end
   end
 end
