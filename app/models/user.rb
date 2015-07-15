2747ae3e368a71abf20b2c79279f091458dfc7b9
Loraine <loraine@lorainekv.com>
2015-07-15 13:54:01 -0700

    Added rspec validation for uniquness of password field.)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :products
   has_many :orders
-
+  has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,17 +17,23 @@ RSpec.describe User, type: :model do
   end
 
   before do
-    @user = User.create(username: "moo", email: "goats@coats.com")
+    @user = User.create(username: "moo", email: "goats@coats.com", password: "unigoats")
     @other_user = @user.dup
     @other_user.save
   end
 
   it "requires that a username be unique" do
     expect(@other_user).to_not be_valid
+    expect(@other_user.errors[:username]).to include("has already been taken")
   end
 
   it "requires that a email be unique" do
     expect(@other_user).to_not be_valid
+      expect(@other_user.errors[:email]).to include("has already been taken")
   end
 
+  it "Expects if passwords to not match the user is not valid" do
+    @user2 = User.create(username: "moo", email: "goats@coats.com", password: "unigoats", password_confirmation: "unigoatsss")
+    expect(@user2).to_not be_valid
+  end
 end
