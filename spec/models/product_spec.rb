837ab7bf4ea4593c45b583f20a0de4d08cec2063
Loraine <loraine@lorainekv.com>
2015-07-15 16:01:00 -0700

    Finished Product Model Validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,21 +4,16 @@ RSpec.describe Product, type: :model do
 
   it "Requires that a product name be present" do
       product = Product.create
-
       expect(product).to_not be_valid
       expect(product.errors.keys).to include(:name)
   end
 
-  before do
-    @product = Product.create(name: "fancy cat fedora", price:1.5)
-    @other_product = @product.dup
-    @other_product.save
-
-  end
-
   it "requires that a product name be unique" do
-    expect(@other_product).to_not be_valid
-    expect(@other_product.errors[:name]).to include("has already been taken")
+    product = Product.create(name: "fancy cat fedora", price:1.5, user_id: 2)
+    other_product = product.dup
+    other_product.save
+    expect(other_product).to_not be_valid
+    expect(other_product.errors[:name]).to include("has already been taken")
   end
 
   it "requires that the price is present" do
@@ -30,6 +25,16 @@ RSpec.describe Product, type: :model do
   it "expects a price to be a number" do
     bad_price = Product.create(price: "b")
     expect(bad_price).to_not be_valid
+  end
+
+  it "expects price to be greater than zero" do
+    product=Product.new(name: "goat poncho", price: -5, user_id: 1)
+    expect(product).to_not be_valid
+  end
+
+  it "requires that a product must belong to a user" do
+    product=Product.new(name: "kitten mittens", price: 3)
+    expect(product).to_not be_valid
   end
 
 end
