1619469d7e48ae5bde0463038e59b1eb2bdd22e1
Loren Wang <wang131.131@gmail.com>
2015-07-15 14:31:22 -0700

    order_item validations and tests

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,6 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Order, type: :model do
   describe "order model validations" do
     it "must have one or more order items" do
       order = Order.new(id: 1)
-      order_item = OrderItem.new(id: 1, quantity: 3, order_id: 1)
-      order_item.save
+      order_item = OrderItem.create(id: 1, quantity: 3, order_id: 1)
       order_item2 = OrderItem.create(id: 2, quantity: 4, order_id: 1)
       order.save
 
