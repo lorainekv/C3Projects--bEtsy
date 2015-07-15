b107d5bbffc66bfc8189e858257fb9f592c1cc80
Loraine <loraine@lorainekv.com>
2015-07-15 14:31:38 -0700

    WIP product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :name, presence: true, uniqueness: true
   
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
 
+  it "Requires that a product name be present" do
+      product = Product.create
+
+      expect(product).to_not be_valid
+      expect(product.errors.keys).to include(:name)
+  end
+
+  before do
+    @product = Product.create(name: "fancy cat fedora")
+    @other_product = @product.dup
+    @other_product.save
+  end
+
+  it "requires that a product name be unique" do
+    expect(@other_product).to_not be_valid
+    expect(@other_product.errors[:name]).to include("has already been taken")
+  end
+
+  it "expects a price to be present" do
+  end
+
 end
