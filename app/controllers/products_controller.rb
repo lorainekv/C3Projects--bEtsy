c19f5da26d5192ac3ce56d7726b88ab63ec3330f
Loren Wang <wang131.131@gmail.com>
2015-07-16 15:24:46 -0700

    started on products & categories controllers specs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def create_params
+    params.permit(product: [:name, :price, :description, :stock, :photo_url, :user_id, :product_id])
+  end
 end
