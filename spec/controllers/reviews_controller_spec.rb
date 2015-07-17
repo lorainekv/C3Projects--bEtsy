5842b9892531a2ccf47eabf9c4d96288480eece3
Loraine <loraine@lorainekv.com>
2015-07-16 22:09:33 -0700

    Added a new method spec for the reviews controller.

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 require 'pry'
 RSpec.describe ReviewsController, type: :controller do
 
+  describe "GET #new" do
+    it "responds with an HTTP 200 status" do
+      get :new, product_id: 1
+
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+  end
+
   describe "POST #create" do
     context "Valid reviews params do" do
 
