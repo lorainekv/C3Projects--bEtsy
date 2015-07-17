75586f55effeba4fbaac16b0766359240a7c89bb
Loren Wang <wang131.131@gmail.com>
2015-07-17 01:13:57 -0700

    category controller specs wip

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -8,11 +8,25 @@ RSpec.describe CategoriesController, type: :controller do
        expect(response).to render_template("index")
     end
 
+    it "loads all categories into @categories" do
+      category1, category2 = Category.create(:name => "First Category Name"), Category.create(:name => "2nd Category Name")
+      get :index
+      expect(assigns(:categories)).to match_array([category1, category2])
+    end
+  end
+
+  describe "GET #show" do
+    it "renders the :show view for each category" do
+       get :show
+       expect(response).to render_template("show")
+    end
+
     it "loads a category into @category" do
       category1 = Category.create(:name => "First Category Name")
-      get :index
+      get :show
       expect(assigns(:categories)).to eq([category1])
     end
+
   end
 
 
