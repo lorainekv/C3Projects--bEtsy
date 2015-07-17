f5980f80e22ab15254508ef76cdf13f62e64aab3
Loren Wang <wang131.131@gmail.com>
2015-07-16 17:55:29 -0700

    category controller specs

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
 
+  describe "GET index" do
+    it "renders the :index view for all categories" do
+       get :index
+       expect(response).to render_template("index")
+    end
+
+    it "loads a category into @category" do
+      category1 = Category.create(:name => "First Category Name")
+      get :index
+      expect(assigns(:categories)).to eq([category1])
+    end
+  end
+
+
 end
