e8ce7728f4a8048a8ebc74e19d16d6c5e440b7b2
Loraine <loraine@lorainekv.com>
2015-07-16 22:05:59 -0700

    Finally got the spec to work. It was confusing since I needed to pass in a product id, in addition to the review params hash.

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,26 +1,21 @@
 require 'rails_helper'
-
+require 'pry'
 RSpec.describe ReviewsController, type: :controller do
+
   describe "POST #create" do
-    context "Valid review params" do
+    context "Valid reviews params do" do
+
       let (:review_params) do
-        {
-          review: {
-            rating: 4,
-            text_review: "I like turtles.",
-            product_id: 5
-          }
-        }
-      end
+              {
+                rating: 4,
+                text_review: "I like turtles.",
+                product_id: 1}
+            end
 
       it "creates a review" do
-        post :create, review_params
-
-        expect(Review.count).to eq(1)
-      end # it
-
+              post :create, :product_id => 1, :review => review_params
+              expect(Review.count).to eq(1)
+        end # it
     end # context
-
-
-  end # describe
-end # rspec
+  end # second describe
+end # first describe
