6b0c2939ac46f560531f4d445864b84f5aa5febe
Lila Garcia <lilagrc@gmail.com>
2015-07-20 14:17:27 -0700

    Added functionality for authenticated user to add a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,30 @@
 class CategoriesController < ApplicationController
+  before_action :require_login, only: [:new, :create]
   def index
     @categories = Category.all
   end
 
   def show
     @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(create_params[:category])
+
+    if @category.save
+      redirect_to dashboard_path(session[:user_id])
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def create_params
+    params.permit(category:[:name])
   end
 end
