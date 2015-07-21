741552d8e9bd636948e3685767d5a9c8ff217176
Loraine <loraine@lorainekv.com>
2015-07-20 18:13:54 -0700

    Sessions controller spec for the create method.

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+
+  describe "POST #create" do
+
+  let (:session_params) do
+          {
+            email: "unigoat@unigoat.edu",
+            password: "unigoat"}
+        end
+
+  it "creates an authenticated session" do
+    @user = User.create(username: "unigoat", email: "unigoat@unigoat.edu", password: "unigoat", password_confirmation: "unigoat")
+    post :create, :session => session_params
+
+    binding.pry
+
+    expect(session[:user_id]).to eq(1)
+    end
+
+
+  end
+
+end
