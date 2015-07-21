77359eb14c9cf258bf681728feced56626776d38
Loraine <loraine@lorainekv.com>
2015-07-20 21:19:07 -0700

    delete #destroy spec added for sessions_controller.rb

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,23 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  before(:each) do
+    @user = User.create(username: "unigoat", email: "unigoat@unigoat.edu", password: "unigoat", password_confirmation: "unigoat")
+  end
 
   describe "POST #create" do
 
-  let (:session_params) do
-          {
-            email: "unigoat@unigoat.edu",
-            password: "unigoat"}
-        end
+    let (:session_params) do
+            {
+              email: "unigoat@unigoat.edu",
+              password: "unigoat"}
+          end
 
-  it "creates an authenticated session" do
-    @user = User.create(username: "unigoat", email: "unigoat@unigoat.edu", password: "unigoat", password_confirmation: "unigoat")
-    post :create, :session => session_params
+    it "creates an authenticated session" do
+      @user = User.create(username: "unigoat", email: "unigoat@unigoat.edu", password: "unigoat", password_confirmation: "unigoat")
+      post :create, :session => session_params
 
-    expect(session[:user_id]).to eq(1)
-    end
-
-
+      expect(session[:user_id]).to eq(1)
+      end
   end
 
+  describe "DELETE #destroy" do
+    it "allows user to log out" do
+      session[:user_id] = @user.id
+      delete :destroy
+      expect(session[:user_id]).to eq(nil)
+    end
+  end
 end
