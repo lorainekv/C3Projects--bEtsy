ba16cae62d34aaef6dc45cdb0770ebc0f90f03ba
Loraine <loraine@lorainekv.com>
2015-07-21 13:56:30 -0700

    Wrote spec for changing status to paid during transaction.

---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe OrdersController, type: :controller do
+
+  describe "GET #show " do
+    # it "allows vendors to see their orders" do
+    #   get :show, :order_id => 1
+    #   expect(response).to be_success
+    # end
+  end
+
+  describe "PATCH #update" do
+    before(:each) do
+      @order = Order.create(status: "pending")
+      item = OrderItem.create(quantity: 1, order_id: 1, product_id: 1)
+      session[:order_id] = @order.id
+    end
+
+    it "changes the order status to 'paid'" do
+      patch :update, :id => session[:order_id]
+      @order.reload
+      expect(@order.status).to eq("paid")
+    end
+
+  
+
+  end
+
+
+
+
+end
