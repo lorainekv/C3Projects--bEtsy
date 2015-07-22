c26c27c9f5a1f8ff254adb6ead188e332bb75d5c
Loren Wang <wang131.131@gmail.com>
2015-07-21 21:13:40 -0700

    tests for orders show/index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,50 +1,50 @@
 class OrdersController < ApplicationController
 
-def new
+  def new
 
-end
+  end
 
-def index
-  @orders = Order.all
-end
-
-
-def edit
-  @order = Order.find(session[:order_id])
-  render :edit
-end
-
-def show
-  @order = Order.find(params[:order_id])
-  render :show
-end
-
-def update
-  @order = Order.find(session[:order_id])
-  @order.update(create_params[:checkout])
-  if @order.order_items.length > 0
-    @order.status = 'paid'
-    @order.save
-
-    update_stock
-
-    # Clear the session's order_id so any new items get a new order
-    session[:order_id] = nil
-    @time = Time.now.localtime
+  def index
+    @orders = Order.all
+  end
 
 
-    render '/orders/confirmation'
-
-  else
-    flash.now[:error] = "Order must have at least one item."
+  def edit
+    @order = Order.find(session[:order_id])
     render :edit
   end
-end
 
-private
+  def show
+    @order = Order.find(params[:order_id])
+    render :show
+  end
 
-def create_params
-  params.permit(checkout: [:name, :email, :address, :cc4, :expiry_date])
-end
+  def update
+    @order = Order.find(session[:order_id])
+    @order.update(create_params[:checkout])
+    if @order.order_items.length > 0
+      @order.status = 'paid'
+      @order.save
 
-end
+      update_stock
+
+      # Clear the session's order_id so any new items get a new order
+      session[:order_id] = nil
+      @time = Time.now.localtime
+
+
+      render '/orders/confirmation'
+
+    else
+      flash.now[:error] = "Order must have at least one item."
+      render :edit
+    end
+  end
+
+  private
+
+  def create_params
+    params.permit(checkout: [:name, :email, :address, :cc4, :expiry_date])
+  end
+
+  end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,18 +2,54 @@ require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
 
-  describe "GET #show " do
-    # it "allows vendors to see their orders" do
-    #   get :show, :order_id => 1
-    #   expect(response).to be_success
-    # end
+  describe "GET #index" do
+
+    it "renders the :index view for a User's orders" do
+       get :index
+       expect(response).to render_template("index")
+    end
+
+    it "loads all orders into @orders" do
+      order, order2 = Order.create(id: 1), Order.create(id:2)
+      item, item2 = OrderItem.create(quantity: 1, order_id: 1, product_id: 1), OrderItem.create(quantity: 1, order_id: 2, product_id: 1)
+      get :index
+      expect(assigns(:orders)).to match_array([order, order2])
+    end
+
   end
+
+  describe "GET #show " do
+    before(:each) do
+      @order = Order.create(id: 1)
+      @item = OrderItem.create(quantity: 1, order_id: 1, product_id: 1)
+      session[:order_id] = @order.id
+    end
+
+    after :each do
+      @order.destroy
+    end
+
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "allows vendors to see their orders" do
+      get :show, id: @order.id, order_id: @item.order_id
+      expect(assigns(:order)).to eq(@order)
+    end
+  end
+
 
   describe "PATCH #update" do
     before(:each) do
-      @order = Order.create(status: "pending")
-      item = OrderItem.create(quantity: 1, order_id: 1, product_id: 1)
+      @order = Order.create(id: 1, status: "pending")
+      @item = OrderItem.create(quantity: 1, order_id: 1, product_id: 1)
       session[:order_id] = @order.id
+    end
+
+    after :each do
+      @order.destroy
     end
 
     it "changes the order status to 'paid'" do
@@ -28,8 +64,6 @@ RSpec.describe OrdersController, type: :controller do
     end
 
   end
-
-
 
 
 end
