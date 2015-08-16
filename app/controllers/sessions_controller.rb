c86505ed545c007d58a7b70f3feff21ea95af921
Loraine <loraine@lorainekv.com>
2015-08-16 15:06:10 -0700

    No body method refactor.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
 class SessionsController < ApplicationController
   before_action :logged_in_user, only: [:new]
 
-  def new
-
-  end
+  def new; end
 
   def create
     @user = User.find_by_email(params[:session][:email])
