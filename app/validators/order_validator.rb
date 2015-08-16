761931299f4d1e94ee1153d6e8a1d7bff6f8718f
Loren Wang <wang131.131@gmail.com>
2015-08-16 15:24:01 -0700

    remove old comments

--- app/validators/order_validator.rb
+++ app/validators/order_validator.rb
@@ -1,6 +1,5 @@
 class OrderValidator < ActiveModel::Validator
   def validate(order)
-    # Do nothing for now.
     # We must create an order before we create an order_item,
     # so the initial order must temporarily not have any order items
     unless order.order_items.length >= 1
