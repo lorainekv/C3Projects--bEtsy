02cb4e271d99aaa34b1f0522a86f0547532787e9
Loraine <loraine@lorainekv.com>
2015-08-16 17:26:44 -0700

    Refactored welcome controller spec with Factory Girl.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
 
-it "loads all active products into @product" do
-  product1 = Product.create(:name => "1st Product Name", :price => 2, :user_id => 3, :status => "active")
-  product2 =  Product.create(:name => "2nd Product Name", :price => 5, :user_id => 4, :status => "retired")
-  get :index
+  it "loads all active products into @product" do
+    product1 = create :product
+    product2 =  create :product, :name => "2nd Product Name", status: "retired"
+    get :index
 
-  expect(assigns(:products)).to match_array([product1])
+    expect(assigns(:products)).to match_array([product1])
 
-end
-
-
+  end
 end
