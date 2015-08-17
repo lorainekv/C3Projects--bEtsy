aec104f6a894c7b6e84c625f2792b48a610645bd
Loraine <loraine@lorainekv.com>
2015-08-16 17:13:10 -0700

    Refactored products_controller with Factory Girl.

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,31 +2,26 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
   describe "GET #index" do
-
     it "loads just active products into @products" do
-      product1 = Product.create(:name => "1st Product Name", :price => 2, :user_id => 3, :status => "active")
-      product2 =  Product.create(:name => "2nd Product Name", :price => 5, :user_id => 4, :status => "retired")
+      product1 = create :product
+      product2 =  create :product, name: "cow shades", status: "retired"
+
       get :index
+
       expect(assigns(:products)).to match_array([product1])
     end
   end
 
+  #Since the following methods require a user to be logged in...
+  before(:each) do
+    @user = create :user
+    session[:user_id] = @user.id
+  end
+
   describe "GET#New" do
-    it "saves a new blank instance of product in a variable" do
-      @product = Product.new(id: 1, name: "some name", price: 1, user_id: 1, status: "active")
-      @product.save
-
+    it "renders the new product template" do
       get :new
-      expect(Product.count).to eq 1
-    end
-
-    it "saves a new blank instance of review in a variable" do
-      @product = Product.new(id: 7, name: "some name", price: 1, user_id: 1)
-      @product.save
-      @review = Review.new(rating: 1, text_review: "twas good", product_id: 7)
-      @review.save
-      get :new, :product_id => @product.id
-      expect(@review.product_id).to eq 7
+      expect(response).to render_template(:new)
     end
   end
 
@@ -34,11 +29,6 @@ RSpec.describe ProductsController, type: :controller do
     context "valid params" do
       let (:params) do {product: {id: 1, name: "a name", price: 1, user_id: 1, status: "active"}}
       end
-
-      before :each do
-        @user = User.create( id: 1, username: "someone", email: "an email is here")
-      end
-
       it "creates a new Product" do
         post :create, params, :user_id => 1
 
@@ -67,7 +57,7 @@ RSpec.describe ProductsController, type: :controller do
   describe "PATCH update" do
     it "updates an existing record" do
       session[:user_id] = 1
-      @product = Product.create(id: 1, name: "some name", price: 4, user_id: 1, status: "active")
+      @product = create :product
 
       patch :update, id: @product.id, product: { id: 1, name: "A new name", price: 12, user_id: 1}
       @product.reload
